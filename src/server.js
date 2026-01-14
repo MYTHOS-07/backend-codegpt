c212913f65f04d193ceda64a1b4b6dbf2271f431
MYTHOS-07 <giripunit1000@gmail.com>
2026-01-14 21:35:13 +0545

    server.js

--- src/server.js
+++ src/server.js
@@ -1,33 +1,33 @@
+import config from "./config/config.js";
 import express from "express";
 import cors from "cors";
-import serverless from "serverless-http";
 import connectDB from "./config/db.js";
 import chatRoutes from "./routes/chatRoute.js";
 import logger from "./middlewares/logger.js";
-import config from "./config/config.js";
 
 const app = express();
+const PORT = config.port;
 
-// Connect to DB
+//DB Connect
 connectDB();
 
-// Middlewares
+//MIDDLEWARES
 app.use(express.json());
 app.use(cors());
 app.use(logger);
 
-// Routes
+//Routes
 app.use("/api", chatRoutes);
 
-// Health check
 app.get("/", (req, res) => {
   res.json({
     name: config.name,
-    port: config.port,
+    port: config.PORT,
     status: "OK",
     version: config.version,
   });
 });
 
-// **DO NOT USE app.listen()**
-export default serverless(app);
+app.listen(PORT, () => {
+  console.log(`Server is up at Port ${PORT}`);
+});
